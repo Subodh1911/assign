13f08bbe47a86cdc0a9f30265c989759562cc0e2
nikhil0235 <rajnikhil705@gmail.com>
2023-12-02 00:42:33 +0530

    shivam

---
+++ src/AdminDashboard.js
@@ -0,0 +1,143 @@
+// AdminDashboard.js
+import React, { useState, useEffect } from 'react';
+
+const AdminDashboard = ({
+ setUsers,
+  users,
+  selectedRows,
+  searchTerm,
+  setSearchTerm,
+  onSelectRow,
+  onDeselectRow,
+  onDeleteSelected,
+  onPageChange,
+  currentPage,
+  itemsPerPage,
+}) => {
+  const [editableRowId, setEditableRowId] = useState(null);
+  const [selectAllChecked, setSelectAllChecked] = useState(false);
+
+  const handleEdit = rowId => {
+    setEditableRowId(rowId);
+  };
+
+  const handleSave = () => {
+    setEditableRowId(null);
+  };
+  const handleDelete = rowId => {
+    const updatedUsers = users.filter(user => user.id !== rowId  );
+    setUsers(updatedUsers); // Update the users state to reflect the deletion
+    onDeleteSelected([rowId]);
+  };
+
+  const handlePageChange = page => {
+    onPageChange(page);
+  };
+
+  useEffect(() => {
+    // Update the state of "Select All" checkbox when all rows on the current page are selected
+    setSelectAllChecked(selectedRows.length === itemsPerPage);
+  }, [selectedRows, itemsPerPage]);
+  const handleSelectAll = async () => {
+    const idsOnCurrentPage = users
+      .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
+      .map(user => user.id);
+      console.log(idsOnCurrentPage)
+
+    if (selectAllChecked) {
+      // Deselect all rows if "Select All" is checked
+      for (const rowId of idsOnCurrentPage) {
+        await onDeselectRow(rowId);
+      }
+    } else {
+      // Select all rows if "Select All" is not checked
+      for (const rowId of idsOnCurrentPage) {
+        await onSelectRow(rowId);
+      }
+    }
+    // Toggle the state of "Select All" checkbox
+    setSelectAllChecked(!selectAllChecked);
+  };
+  return (
+    <div>
+      <input
+        className='search-icon'
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input
+                type="checkbox"
+                onChange={handleSelectAll}
+                checked={selectAllChecked}
+              />
+            </th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id} className={selectedRows.includes(user.id) ? 'selected' : ''}>
+              <td>
+                <input
+                  type="checkbox"
+                  onChange={() => {
+                    selectedRows.includes(user.id)
+                      ? onDeselectRow(user.id)
+                      : onSelectRow(user.id);
+                  }}
+                  checked={selectAllChecked || selectedRows.includes(user.id)}
+                />
+              </td>
+              <td>{user.id}</td>
+              <td>{editableRowId === user.id ? <input type="text" value={user.name} /> : user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+              <td>
+                {editableRowId === user.id ? (
+                  <button className="save" onClick={handleSave}>
+                    Save
+                  </button>
+                ) : (
+                  <>
+                    <button className="edit" onClick={() => handleEdit(user.id)}>
+                      Edit
+                    </button>
+                    <button className="delete" onClick={() => handleDelete(user.id)}>
+                      Delete
+                    </button>
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={() => handlePageChange(1)}>&lt;&lt; First</button>
+        <button onClick={() => handlePageChange(currentPage - 1)}>&lt; Previous</button>
+        <span>{`Page ${currentPage}`}</span>
+        <button onClick={() => handlePageChange(currentPage + 1)}>Next &gt;</button>
+        <button onClick={() => handlePageChange(Math.ceil(46/10
+         ))}>
+          Last &gt;&gt;
+        </button>
+      </div>
+      <button onClick={onDeleteSelected} className="delete-selected">
+        Delete Selected
+      </button>
+    </div>
+  );
+};
+
+export default AdminDashboard;
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState, useEffect } from 'react';
+import './App.css'; // You can create this CSS file for styling
+import AdminDashboard from './AdminDashboard';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [len, setLen] = useState(0);
+  useEffect(() => {
+  // Fetch data from the API endpoint
+    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+        setFilteredUsers(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    useEffect(() => {
+        setLen(users.length)
+        }, []);
+    console.log("len is " + len )
+  // Handle search
+  useEffect(() => {
+    const filteredResults = users.filter(user =>
+      Object.values(user).some(value =>
+        value.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setFilteredUsers(filteredResults);
+    setCurrentPage(1); // Reset to the first page after a new search
+  }, [searchTerm, users]);
+
+  // Calculate current items to display on the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
+  // Handle pagination
+  const paginate = page => setCurrentPage(page);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AdminDashboard
+        len={len}
+        users={currentItems}
+        setUsers={setUsers}
+        selectedRows={selectedRows}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        onSelectRow={rowId => setSelectedRows([...selectedRows, rowId])}
+        onDeselectRow={rowId =>
+          setSelectedRows(selectedRows.filter(id => id !== rowId))
+        }
+        onDeleteSelected={() => console.log('Deleting selected rows:', selectedRows)}
+        onPageChange={paginate}
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+      />
     </div>
   );
 }
